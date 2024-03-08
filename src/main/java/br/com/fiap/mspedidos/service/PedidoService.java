c81bdd6dcbdbd86bad89cee96e09a8ead58cd557
bruno-gds <brunog.damascena@gmail.com>
2024-03-08 14:43:04 -0300

    calculando valor total dos produtos e atualizando estoque

--- src/main/java/br/com/fiap/mspedidos/service/PedidoService.java
+++ src/main/java/br/com/fiap/mspedidos/service/PedidoService.java
@@ -42,6 +42,11 @@ public class PedidoService {
             throw new NoSuchElementException("Um ou mais produtos não estão disponíveis.");
         }
 
+        double valorTotal = calcularValorTotal(pedido.getItensPedido());
+        pedido.setValorTotal(valorTotal);
+
+        atualizarEstoqueProdutos(pedido.getItensPedido());
+
         return pedidoRepository.save(pedido);
     }
 
@@ -76,5 +81,53 @@ public class PedidoService {
         }
 
         return true;
+    }
+
+    private double calcularValorTotal(List<ItemPedido> itensPedido) {
+
+        double valorTotal = 0.0;
+
+        for (ItemPedido itemPedido : itensPedido) {
+            Integer idProduto = itemPedido.getIdProduto();
+            int quantidade = itemPedido.getQuantidade();
+
+            ResponseEntity<String> response = restTemplate.getForEntity(
+                    "http://localhost:8080/api/produtos/{produtoId}",
+                    String.class,
+                    idProduto
+            );
+
+            if (response.getStatusCode() == HttpStatus.OK) {
+
+                try {
+                    JsonNode produtoJson = objectMapper.readTree(response.getBody());
+                    double preco = produtoJson.get("preco").asDouble();
+
+                    valorTotal += (preco * quantidade);
+                } catch (IOException e) {
+                    // TODO: tratar depois
+                }
+            }
+        }
+
+        return valorTotal;
+    }
+
+    private void atualizarEstoqueProdutos(List<ItemPedido> itensPedidos) {
+        for (ItemPedido itemPedido : itensPedidos) {
+            Integer idProduto = itemPedido.getIdProduto();
+            int quantidade = itemPedido.getQuantidade();
+
+            restTemplate.put(
+                    "http://localhost:8080/api/produtos/atualizar/estoque/{produtoId}/{quantidade}",
+                    null,
+                    idProduto,
+                    quantidade
+            );
+        }
+    }
+
+    public List<Pedido> listarPedidos() {
+        return pedidoRepository.findAll();
     }
 }
